655628a099648936cb25c9fb4fa1e9058446d7cb
Sayana <tssayana2001@gmail.com>
2023-04-10 12:47:27 +0800

    rs

---
+++ Models/WorksContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioApi.Models;
+public class WorksContext : DbContext
+{
+    public WorksContext(DbContextOptions<PostContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<WorksItem> WorksItems { get; set; } = null!;
+}
